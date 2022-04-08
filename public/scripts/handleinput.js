e5f57cf98e815d7888358e27a340d581c9c6422c
DyB07 <dhaniel.y.bawias@gmail.com>
2022-04-08 21:40:05 +0800

    mengupload file challange 4

---
+++ public/scripts/handleinput.js
@@ -0,0 +1,38 @@
+/* const textinput = document.getElementById('textinput')
+const hasil_ti = document.getElementById('hasil_ti')
+textinput.addEventListener('change', (e) => {
+    e.preventDefault()
+    hasil_ti.innerHTML = e.target.value
+    filterCar(e.target.value)
+    }
+) */
+
+const FilterAvailable = () => {
+    let available = document.getElementById('select').value
+    return available;
+}
+
+const FilterDate = () => {
+    let Tanggal_value = document.getElementById('select-tanggal').value
+    return Tanggal_value;
+}
+
+const FilterTime = () => {
+    let Time = document.getElementById('select-waktu').value
+    return Time;
+}
+
+const FilterCapacity = () => {
+    let Capacity = document.getElementById('penumpang').value
+    return Capacity;
+}
+
+
+const FilterTombol = document.getElementById('tombol')
+FilterTombol.addEventListener('click', (e) => {
+    filterCar(
+        FilterAvailable(), 
+        FilterDate(), 
+        FilterTime(), 
+        FilterCapacity())
+})
